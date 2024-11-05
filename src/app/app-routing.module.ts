b2cace2ca06b72c8f6dd87a957c243d6899c2c67
João Victor Álvares <80368253+jvsouza17@users.noreply.github.com>
2024-11-05 09:32:26 -0300

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -13,19 +13,19 @@ import { ExameDetalheComponent } from './componentes/exames-detalhe/exames-detal
 import { CartaoVacinasComponent } from './componentes/cartao-vacinas/cartao-vacinas.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent, pathMatch: 'full',
+  { path: '', component: HomeComponent, pathMatch: 'full', canActivate: [UsuarioAutenticadoGuard],
     children: [{ path: '', component: HomeComponent }],
   },
   { path: 'login', component: LoginComponent, canActivate: [UsuarioNaoAutenticadoGuard]},  // Rota para o componente de login
 
   { path: 'cadastro', component: CadastroComponent, canActivate: [UsuarioNaoAutenticadoGuard]},  // Rota para o componente de cadastro
-  { path: 'home', component: HomeComponent},
-  { path: 'consultas', component: ConsultasComponent},
-  { path: 'exames', component: ExamesComponent},
-  { path: 'agendar-consulta', component: AgendarConsultaComponent},
-  { path: 'meus-agendamentos', component: MeusAgendamentosComponent},
-  { path: 'exames-detalhes', component: ExameDetalheComponent},
-  { path: 'cartao-vacinas', component: CartaoVacinasComponent},
+  { path: 'home', component: HomeComponent, canActivate: [UsuarioAutenticadoGuard]},
+  { path: 'consultas', component: ConsultasComponent, canActivate: [UsuarioAutenticadoGuard]},
+  { path: 'exames', component: ExamesComponent, canActivate: [UsuarioAutenticadoGuard]},
+  { path: 'agendar-consulta', component: AgendarConsultaComponent, canActivate: [UsuarioAutenticadoGuard]},
+  { path: 'meus-agendamentos', component: MeusAgendamentosComponent, canActivate: [UsuarioAutenticadoGuard]},
+  { path: 'exames-detalhes', component: ExameDetalheComponent, canActivate: [UsuarioAutenticadoGuard]},
+  { path: 'cartao-vacinas', component: CartaoVacinasComponent, canActivate: [UsuarioAutenticadoGuard]},
   { path: '**', redirectTo: '/login'},  // Rota coringa redireciona para login em caso de rota não encontrada
 ];
 
